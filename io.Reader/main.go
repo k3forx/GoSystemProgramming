9e9991dbbe8a7071f4baf8120ded3aed3e1ee9b6
Kanata Miyahana <afiri.bupa@gmail.com>
2022-12-20 00:08:18 +0900

    png chunk

--- io.Reader/main.go
+++ io.Reader/main.go
@@ -12,9 +12,25 @@ import (
 )
 
 func main() {
+	fmt.Println("-----------------------------------")
 	fileInputOutput()
+
+	fmt.Println("-----------------------------------")
 	networkInputOutput()
+
+	fmt.Println("-----------------------------------")
 	endianTransform()
+
+	fmt.Println("-----------------------------------")
+	file, err := os.Open("PNG_transparency_demonstration_1.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	chunks := readChunks(file)
+	for _, chunk := range chunks {
+		dumpChunk(chunk)
+	}
 }
 
 func fileInputOutput() {
@@ -50,4 +66,36 @@ func endianTransform() {
 	// エンディアンの変換
 	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
 	fmt.Printf("data: %d\n", i)
+}
+
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+}
+
+func readChunks(file *os.File) []io.Reader {
+	var chunks []io.Reader
+
+	// 最初の8バイトを飛ばす
+	file.Seek(8, 0)
+	var offset int64 = 8
+
+	for {
+		var length int32
+		// 32ビット (4byte) 分だけPNGのバイナリデータを読み込む
+		// PNGのバイナリデータの形式から最初の4byteはデータの長さの情報になっている
+		if err := binary.Read(file, binary.BigEndian, &length); err == io.EOF {
+			break
+		}
+		chunks = append(chunks,
+			io.NewSectionReader(file, offset, int64(length)+12))
+		// 次のチャンクの先頭に移動
+		// 現在位置は長さを読み終わった箇所なので
+		// チャンク名 (4バイト) + データ長 + CRC (4バイト) 先に移動
+		offset, _ = file.Seek(int64(length+8), 1)
+	}
+	return chunks
 }
