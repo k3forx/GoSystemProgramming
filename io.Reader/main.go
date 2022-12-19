9a8daac15c03d1b0b7852573f5b0690e2fd773b0
Kanata Miyahana <afiri.bupa@gmail.com>
2022-12-20 00:30:27 +0900

    embed text

--- io.Reader/main.go
+++ io.Reader/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"net"
 	"net/http"
@@ -29,6 +30,29 @@ func main() {
 	defer file.Close()
 	chunks := readChunks(file)
 	for _, chunk := range chunks {
+		dumpChunk(chunk)
+	}
+
+	fmt.Println("-----------------------------------")
+	newFile, err := os.Create("PNG_transparency_demonstration_secret.png")
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	chunks = readChunks(file)
+	// シグニチャを書き込み
+	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+	// 先頭に必要なIHDRチャンクを書き込み
+	io.Copy(newFile, chunks[0])
+	// テキストチャンクを追加
+	io.Copy(newFile, textChunk("Lambda Note++"))
+	// 残りのチャンクを追加
+	for _, chunk := range chunks[1:] {
+		io.Copy(newFile, chunk)
+	}
+
+	newChunks := readChunks(newFile)
+	for _, chunk := range newChunks {
 		dumpChunk(chunk)
 	}
 }
@@ -74,6 +98,12 @@ func dumpChunk(chunk io.Reader) {
 	buffer := make([]byte, 4)
 	chunk.Read(buffer)
 	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+
+	if bytes.Equal(buffer, []byte("teXt")) {
+		rawText := make([]byte, length)
+		chunk.Read(rawText)
+		fmt.Println(string(rawText))
+	}
 }
 
 func readChunks(file *os.File) []io.Reader {
@@ -98,4 +128,18 @@ func readChunks(file *os.File) []io.Reader {
 		offset, _ = file.Seek(int64(length+8), 1)
 	}
 	return chunks
+}
+
+func textChunk(text string) io.Reader {
+	byteText := []byte(text)
+	crc := crc32.NewIEEE()
+	var buffer bytes.Buffer
+	binary.Write(&buffer, binary.BigEndian, int32(len(byteText)))
+	// CRC計量とバッファへの書き込みを同時に行うMultiWriter
+	writer := io.MultiWriter(&buffer, crc)
+	io.WriteString(writer, "teXt") //2バイトめの5ビットめを立てる (小文字にする) とプライベート
+
+	writer.Write(byteText)
+	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
+	return &buffer
 }
