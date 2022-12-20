2170594c28ebc1d0003091852ae3c9164ed5d7b1
Kanata Miyahana <afiri.bupa@gmail.com>
2022-12-21 00:12:03 +0900

    solve q3

--- io.Writer/main.go
+++ io.Writer/main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/json"
 	"io"
 	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
 	internetAccess()
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Encoding", "gzip")
+	w.Header().Set("Content-Type", "application/json")
+	// JSON化する元のデータ
+	source := map[string]string{
+		"Hello": "World",
+	}
+
+	compresser := gzip.NewWriter(w)
+	multiWriter := io.MultiWriter(compresser, os.Stdout)
+	encoder := json.NewEncoder(multiWriter)
+	encoder.SetIndent("", "  ")
+	encoder.Encode(source)
+	compresser.Flush()
 }
 
 func internetAccess() {
