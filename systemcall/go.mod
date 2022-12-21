d88daf1af00fa586501e72c75e5f635546b20a9b
Kanata Miyahana <afiri.bupa@gmail.com>
2022-12-21 23:02:55 +0900

    syscall

---
+++ systemcall/go.mod
@@ -0,0 +1,3 @@
+module github.com/k3forx/GoSystemProgramming/systemcall
+
+go 1.19
---
+++ systemcall/main.go
@@ -0,0 +1,12 @@
+package systemcall
+
+import "os"
+
+func main() {
+	file, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.Write([]byte("system call example\n"))
+}
