d44ec84ba1a2b344a7545c09d1748f98be7a6e06
Kanata Miyahana <afiri.bupa@gmail.com>
2023-01-12 23:14:33 +0900

    update main.go

--- signal/main.go
+++ signal/main.go
@@ -1,26 +1,34 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 func main() {
-	ctx := context.Background()
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT)
 
-	sigctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer cancel()
-
-	toctx, cancel2 := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel2()
-
-	select {
-	case <-sigctx.Done():
-		fmt.Println("signal")
-	case <-toctx.Done():
-		fmt.Println("timeout")
-	}
+	fmt.Println("Waiting SIGINT (CTRL+C)")
+	<-signals
+	fmt.Println("SIGINT arrived")
 }
+
+// func main() {
+// 	ctx := context.Background()
+
+// 	sigctx, cancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+// 	defer cancel()
+
+// 	toctx, cancel2 := context.WithTimeout(ctx, 5*time.Second)
+// 	defer cancel2()
+
+// 	select {
+// 	case <-sigctx.Done():
+// 		fmt.Println("signal")
+// 	case <-toctx.Done():
+// 		fmt.Println("timeout")
+// 	}
+// }
